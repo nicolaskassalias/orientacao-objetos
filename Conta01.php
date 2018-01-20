08c49e753571c5bbb3b5cccf26c8b098b83feeb7
php 08 <php.08@lampadinha.cec.ime.usp.br>
2018-01-19 22:35:44 -0200

    orientacao objetos PHP

--- Conta01.php
+++ Conta01.php
@@ -2,12 +2,25 @@
 class Conta
 {
     //atributos
+    public $nome;
     public $numero;
     public $saldo;
 }
 
 $x = new Conta();
-x->numero = 10;
-x->salado = 1500.43;
-echo "\tNúmero da conta x: $x->numero \n";
-echo "\tNúmero do saldo x: $x->saldo \n";
+$x->nome = "Maria da Silva";
+$x->numero = 10;
+$x->saldo = 1500.43;
+echo "\tNome cliente da conta x: $x->nome" . PHP_EOL;
+echo "\tNúmero da conta x: $x->numero" . PHP_EOL;
+echo "\tNúmero do saldo x: $x->saldo" . PHP_EOL;
+//
+$x1 = new Conta();
+$x1->nome = "José Duarte";
+$x1->numero = 20;
+$x1->saldo = 3500.32;
+echo "\n\n";
+//echo "\tNome cliente da conta x1: $x1->nome" . PHP_EOL;
+echo "\tNome cliente da conta x1: $x1->nome\n" ;
+echo "\tNúmero da conta x1: $x1->numero" . PHP_EOL;
+echo "\tNúmero do saldo x1: $x1->saldo" . PHP_EOL;
